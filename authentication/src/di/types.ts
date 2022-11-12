9a3092a5b3dd52bf734a82d598f38ac4b86fb363
numq <numq.dev@gmail.com>
2022-11-12 16:35:52 +0300

    Provide refactoring

--- authentication/src/di/types.ts
+++ authentication/src/di/types.ts
@@ -1,6 +1,8 @@
 export const TYPES = {
-    ConfigProvider: Symbol.for("ConfigProvider"),
-    Database: Symbol.for("Database"),
-    Cache: Symbol.for("Cache"),
-    TokenRepository: Symbol.for("TokenRepository")
+    ConfigProvider: Symbol.for("config_provider"),
+    Database: Symbol.for("database"),
+    Cache: Symbol.for("cache"),
+    TokenRepository: Symbol.for("token_repository"),
+    Authenticate: Symbol.for("authenticate"),
+    VerifyToken: Symbol.for("verify_token")
 }
