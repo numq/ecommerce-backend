69824c514a134be5ba1e6a6184ac23fdc4c216e0
numq <numq.dev@gmail.com>
2022-11-21 20:16:10 +0300

    Provide interactor changes

--- stock/src/interactor/UseCase.ts
+++ stock/src/interactor/UseCase.ts
@@ -1,5 +1,6 @@
 import {Either} from "fp-ts/Either";
+import {Exception} from "../exception/Exception";
 
 export abstract class UseCase<T, R> {
-    abstract execute(arg: T): Either<Error, R>
+    abstract execute(arg: T): Either<Exception, R>
 }
