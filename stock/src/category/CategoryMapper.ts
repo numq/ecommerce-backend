dbaf83261d868fb99eb7aa488a800562fd7f6f57
numq <numq.dev@gmail.com>
2022-11-23 20:06:30 +0300

    Add category mapper

---
+++ stock/src/category/CategoryMapper.ts
@@ -0,0 +1,17 @@
+import {Category} from "./Category";
+import {Category as CategoryMessage} from "../generated/category";
+
+export namespace CategoryMapper {
+    export const entityToMessage = (entity: Category): CategoryMessage => ({
+        id: entity.id,
+        name: entity.name,
+        description: entity.description,
+        imageBytes: Buffer.from(entity.imageBytes)
+    });
+    export const messageToEntity = (message: CategoryMessage): Category => ({
+        id: message.id,
+        name: message.name,
+        description: message.description,
+        imageBytes: new Uint8Array(message.imageBytes)
+    });
+}
