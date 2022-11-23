032ca1a1b77dbfaa7e03e61d2eede2d86eaa918c
numq <numq.dev@gmail.com>
2022-11-23 20:04:34 +0300

    Provide config fixes

--- stock/src/config/Config.ts
+++ stock/src/config/Config.ts
@@ -4,7 +4,7 @@ import {injectable} from "inversify";
 export class Config {
     readonly DATABASE_URL = `${process.env.DATABASE_HOST}://${process.env.DATABASE_HOSTNAME}:${process.env.DATABASE_PORT}`;
     readonly SERVER_URL = `${process.env.SERVER_HOSTNAME}:${process.env.SERVER_PORT}`;
-    readonly DATABASE_NAME = `${process.env.DATABASE_NAME}`;
-    readonly COLLECTION_CATEGORIES = `${process.env.COLLECTION_CATEGORIES}`;
-    readonly COLLECTION_PRODUCTS = `${process.env.COLLECTION_PRODUCTS}`;
+    readonly DATABASE_NAME = process.env.DATABASE_NAME;
+    readonly COLLECTION_CATEGORIES = process.env.COLLECTION_CATEGORIES;
+    readonly COLLECTION_PRODUCTS = process.env.COLLECTION_PRODUCTS;
 }
