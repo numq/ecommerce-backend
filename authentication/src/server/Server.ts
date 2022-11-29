cac4317d08fef09161b86ed863c722d39127670d
numq <numq.dev@gmail.com>
2022-11-29 17:22:30 +0300

    Add server

---
+++ authentication/src/server/Server.ts
@@ -0,0 +1,23 @@
+import {Server as GrpcServer, ServerCredentials} from "@grpc/grpc-js";
+import {inject, injectable} from "inversify";
+import {Types} from "../di/types";
+import {Config} from "../config/Config";
+
+@injectable()
+export class Server {
+    constructor(
+        @inject(Types.app.config) private readonly config: Config,
+        @inject(Types.account.service) private readonly accountService: AccountServiceServer
+    ) {
+    }
+
+    async launch(bind: (server: GrpcServer) => void) {
+        const server: GrpcServer = new GrpcServer();
+        bind(server);
+        server.bindAsync(this.config.SERVER_URL, ServerCredentials.createInsecure(), (error, port) => {
+            if (error) console.error(error);
+            server.start();
+            console.log(`Server running on port: ${port}`);
+        });
+    };
+}
