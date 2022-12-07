71c9922a21a35d76fe5ad28ea15a1193ea5f24c5
numq <numq.dev@gmail.com>
2022-12-07 18:08:49 +0300

    Add cart service

---
+++ cart/src/cart/CartService.ts
@@ -0,0 +1,53 @@
+import {inject, injectable} from "inversify";
+import {Types} from "../di/types";
+import {IncreaseItemQuantity} from "./IncreaseItemQuantity";
+import {GetCart} from "./GetCart";
+import {sendUnaryData, ServerUnaryCall, UntypedHandleCall} from "@grpc/grpc-js";
+import {response} from "../response";
+import {ItemMapper} from "./ItemMapper";
+import {ClearCart} from "./ClearCart";
+import {DecreaseItemQuantity} from "./DecreaseItemQuantity";
+import {
+    CartServiceServer,
+    ClearCartRequest,
+    ClearCartResponse,
+    DecreaseItemQuantityRequest,
+    DecreaseItemQuantityResponse,
+    GetCartRequest,
+    GetCartResponse,
+    IncreaseItemQuantityRequest,
+    IncreaseItemQuantityResponse
+} from "../generated/cart";
+
+@injectable()
+export class CartService implements CartServiceServer {
+    [name: string]: UntypedHandleCall | any;
+
+    constructor(
+        @inject(Types.cart.getCart) private readonly getCartUseCase: GetCart,
+        @inject(Types.cart.increaseItemQuantity) private readonly increaseItemQuantityUseCase: IncreaseItemQuantity,
+        @inject(Types.cart.decreaseItemQuantity) private readonly decreaseItemQuantityUseCase: DecreaseItemQuantity,
+        @inject(Types.cart.clearCart) private readonly clearCartUseCase: ClearCart
+    ) {
+    }
+
+    getCart(call: ServerUnaryCall<GetCartRequest, GetCartResponse>, callback: sendUnaryData<GetCartResponse>): void {
+        const {cartId} = call.request;
+        response(this.getCartUseCase.execute(cartId), callback, value => ({items: value.map(ItemMapper.entityToMessage)}));
+    }
+
+    clearCart(call: ServerUnaryCall<ClearCartRequest, ClearCartResponse>, callback: sendUnaryData<ClearCartResponse>): void {
+        const {cartId} = call.request;
+        response(this.clearCartUseCase.execute(cartId), callback, value => ({cartId: value}));
+    }
+
+    increaseItemQuantity(call: ServerUnaryCall<IncreaseItemQuantityRequest, IncreaseItemQuantityResponse>, callback: sendUnaryData<IncreaseItemQuantityResponse>): void {
+        const {cartId, itemId} = call.request;
+        response(this.increaseItemQuantityUseCase.execute([cartId, itemId]), callback, value => ({item: ItemMapper.entityToMessage(value)}));
+    }
+
+    decreaseItemQuantity(call: ServerUnaryCall<DecreaseItemQuantityRequest, DecreaseItemQuantityResponse>, callback: sendUnaryData<DecreaseItemQuantityResponse>): void {
+        const {cartId, itemId} = call.request;
+        response(this.decreaseItemQuantityUseCase.execute([cartId, itemId]), callback, value => ({item: value ? ItemMapper.entityToMessage(value) : undefined}));
+    }
+}
