eb4ae2fbad56432b28f12d2328ec961565adcffe
numq <numq.dev@gmail.com>
2022-12-13 19:51:57 +0300

    Provide refactoring

--- authentication/src/di/types.ts
+++ authentication/src/di/types.ts
@@ -1,21 +1,24 @@
 export namespace Types {
     export const app = {
         config: Symbol.for("appConfig"),
+        cache: Symbol.for("appCache"),
         database: Symbol.for("appDatabase"),
         server: Symbol.for("appServer")
     };
+    export const confirmation = {
+        service: Symbol.for("confirmationService"),
+        repository: Symbol.for("confirmationRepository"),
+        sendCredentials: Symbol.for("sendCredentials"),
+        verifyConfirmationCode: Symbol.for("verifyConfirmationCode")
+    };
     export const account = {
         collection: Symbol.for("accountCollection"),
-        repository: Symbol.for("accountRepository"),
-        service: Symbol.for("accountService"),
-        authenticate: Symbol.for("authenticate")
-    };
-    export const confirmation = {
-        service: Symbol.for("confirmationService")
+        repository: Symbol.for("accountRepository")
     };
     export const token = {
+        collection: Symbol.for("tokenCollection"),
         service: Symbol.for("tokenService"),
         repository: Symbol.for("tokenRepository"),
-        verifyToken: Symbol.for("verifyToken")
+        verifyAccessToken: Symbol.for("verifyAccessToken")
     };
 }
