00c4ee8909b0e862aca83fb093f18c98fcc84d3c
numq <numq.dev@gmail.com>
2022-12-17 12:43:37 +0300

    Provide config refactoring

--- authentication/src/config/Config.ts
+++ authentication/src/config/Config.ts
@@ -1,23 +1,8 @@
 import {injectable} from "inversify";
-import {Role} from "../account/Role";
-import {Secret} from "jsonwebtoken";
 
 @injectable()
 export class Config {
     readonly SECRET_KEY = process.env.SECRET_KEY!;
-    readonly SECRET_KEY_STAFF = process.env.SECRET_KEY_STAFF!;
-    readonly SECRET_KEY_COURIER = process.env.SECRET_KEY_COURIER!;
-    readonly SECRET_KEY_CUSTOMER = process.env.SECRET_KEY_CUSTOMER!;
-    readonly secretFromRole = (_: Role): Secret => {
-        switch (_) {
-            case Role.STAFF:
-                return this.SECRET_KEY_STAFF;
-            case Role.COURIER:
-                return this.SECRET_KEY_COURIER;
-            case Role.CUSTOMER:
-                return this.SECRET_KEY_CUSTOMER;
-        }
-    };
     readonly CACHE_URL = `${process.env.CACHE_HOST}://${process.env.CACHE_HOSTNAME}:${process.env.CACHE_PORT}`;
     readonly CACHE_NAME = process.env.CACHE_NAME;
     readonly DATABASE_URL = `${process.env.DATABASE_HOST}://${process.env.DATABASE_HOSTNAME}:${process.env.DATABASE_PORT}`;
