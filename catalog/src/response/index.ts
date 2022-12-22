0ba96ef51a85940c06c2e71cf6f36d411e9343b4
numq <numq.dev@gmail.com>
2022-12-22 15:42:44 +0300

    Provide refactoring

--- catalog/src/response/index.ts
+++ catalog/src/response/index.ts
@@ -1,10 +1,8 @@
 import {TaskEither} from "fp-ts/TaskEither";
-import {taskEither as TE} from "fp-ts";
-import {pipe} from "fp-ts/function";
+import {flow} from "fp-ts/function";
+import {either as E} from "fp-ts";
 import {ResponseError} from "./ResponseError";
 
-export const response = <T, R>(input: TaskEither<Error, T>, callback: (e: Error | null, r: R | null) => void, map: (t: T) => R) => pipe(
-    input,
-    TE.chain((t: T) => TE.tryCatch(() => Promise.resolve(map(t)), () => ResponseError.map)),
-    TE.bimap((e: Error) => callback(e, null), (r: R) => callback(null, r))
-)();
+export const response = <T, R>(input: TaskEither<Error, T>, callback: (e: Error | null, r: R | null) => void, map: (t: T) => R): void => {
+    input().then(flow(E.chain((t: T) => E.tryCatch(() => map(t), () => ResponseError.map)), E.fold((e: Error) => callback(e, null), (r: R) => callback(null, r)))).catch(e => callback(e, null));
+}
