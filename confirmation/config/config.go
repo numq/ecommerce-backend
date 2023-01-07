36bc315507dc487dc55ffe31aa3482111e432636
numq <numq.dev@gmail.com>
2023-01-08 01:22:29 +0300

    Provide confirmation fixes

--- confirmation/config/config.go
+++ confirmation/config/config.go
@@ -8,15 +8,17 @@ import (
 type Config struct {
 	ServiceName   string `mapstructure:"SERVICE_NAME"`
 	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
-	RedisAddress  string `mapstructure:"REDIS_ADDRESS"`
+	RedisHostname string `mapstructure:"REDIS_HOSTNAME"`
+	RedisPort     string `mapstructure:"REDIS_PORT"`
 	ApiKey        string `mapstructure:"API_KEY"`
+	SecretKey     string `mapstructure:"SECRET_KEY"`
 }
 
-func LoadConfig(path string) (config Config, err error) {
-	viper.AddConfigPath(path)
-	viper.SetConfigName("app")
+func LoadConfig(name string) (config Config, err error) {
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("./config")
 	viper.SetConfigType("env")
-	viper.AutomaticEnv()
+	viper.SetConfigName(name)
 	if err = viper.ReadInConfig(); err != nil {
 		log.Fatal(err)
 	}
--- confirmation/main/main.go
+++ confirmation/main/main.go
@@ -5,8 +5,10 @@ import (
 	"confirmation/confirmation"
 	pb "confirmation/generated"
 	"confirmation/server"
+	"confirmation/store"
 	"context"
-	"github.com/go-redis/redis/v9"
+	"flag"
+	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -16,27 +18,32 @@ import (
 )
 
 func main() {
-	cfg, _ := config.LoadConfig(".")
-	if err := os.RemoveAll("./generated"); err != nil {
+	productionMode := flag.Bool("production", false, "enable production mode")
+	flag.Parse()
+	cfgName := func() string {
+		if *productionMode {
+			return "prod"
+		}
+		return "dev"
+	}()
+	cfg, err := config.LoadConfig(cfgName)
+	if err != nil {
 		log.Fatal(err)
 	}
-	if err := os.Mkdir("./generated", os.ModePerm); err != nil {
-		log.Fatal(err)
-	}
-	if err := exec.Command("protoc", "--go_out=generated", "--go-grpc_out=generated", "--proto_path=proto", "proto/*.proto").Run(); err != nil {
-		log.Fatal(err)
-	}
-	redisClient := redis.NewClient(&redis.Options{Addr: cfg.RedisAddress})
-	if _, err := redisClient.Ping(context.Background()).Result(); err != nil {
-		log.Fatal(err)
-	}
-	defer func(redisClient *redis.Client) {
-		err := redisClient.Close()
-		if err != nil {
+	if !*productionMode {
+		if err := os.RemoveAll("./generated"); err != nil {
 			log.Fatal(err)
 		}
-	}(redisClient)
-	accountRepository := confirmation.NewRepository(redisClient)
+		if err := os.Mkdir("./generated", os.ModePerm); err != nil {
+			log.Fatal(err)
+		}
+		if err := exec.Command("protoc", "--go_out=generated", "--go-grpc_out=generated", "--proto_path=proto", "proto/*.proto").Run(); err != nil {
+			log.Fatal(err)
+		}
+	}
+	ctx := context.Background()
+	client := store.NewClient(ctx, fmt.Sprintf("%s:%s", cfg.RedisHostname, cfg.RedisPort))
+	accountRepository := confirmation.NewRepository(client)
 	accountUseCase := confirmation.NewUseCase(accountRepository)
 	accountService := confirmation.NewService(accountUseCase)
 	authInterceptor := server.NewInterceptor("Authorization", func(header string) error {
